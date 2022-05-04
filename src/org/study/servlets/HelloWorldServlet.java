42c4e713c864be945a27c85e6e2abc04040248ac
Francisco Torres <fracmt@gmail.com>
2022-05-04 18:42:45 +0100

    hello world servlet

---
+++ src/org/study/servlets/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package org.study.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+      PrintWriter out = resp.getWriter();
+      out.println("<html>");
+      out.println("<body>");
+      out.println("Hello World with Servlets");
+      out.println("</body>");
+      out.println("</html>");
+
+      System.out.println("Called HelloWorldServlet");
+  }
+}    
